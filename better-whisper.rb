dfe7f33135186392e48776d9f3fae9c402841065
akash-joshi <git@thewriting.dev>
2024-12-11 10:45:23 +0000

    Brew formula update for better-whisper version v0.1.11

--- better-whisper.rb
+++ better-whisper.rb
@@ -5,14 +5,14 @@
 class BetterWhisper < Formula
   desc "A better CLI for whisper-cpp"
   homepage "https://github.com/akash-joshi/better-whisper"
-  version "0.1.10"
+  version "0.1.11"
 
   depends_on "ffmpeg"
   depends_on "whisper-cpp"
 
   on_macos do
-    url "https://github.com/akash-joshi/better-whisper/releases/download/v0.1.10/better-whisper_0.1.10_darwin_all.tar.gz"
-    sha256 "76dd530efcb08671954cdffd8286f430f383bdadd8b2eeed83045435fcdb2f95"
+    url "https://github.com/akash-joshi/better-whisper/releases/download/v0.1.11/better-whisper_0.1.11_darwin_all.tar.gz"
+    sha256 "4e0310cda54511f8deffae80db3428eb8ba782096bb7e3cfc986747874dbd3c0"
 
     def install
       bin.install "better-whisper"
@@ -22,8 +22,8 @@ class BetterWhisper < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/akash-joshi/better-whisper/releases/download/v0.1.10/better-whisper_0.1.10_linux_amd64.tar.gz"
-        sha256 "bf64c1a570c1be3fda64703f2799e44548658a9535a7581fcd4cfe03a50e290d"
+        url "https://github.com/akash-joshi/better-whisper/releases/download/v0.1.11/better-whisper_0.1.11_linux_amd64.tar.gz"
+        sha256 "d30e91a496bd3069ffc8fb6ad19d278d7997e1f78ae12a72b950b3c7996aae86"
 
         def install
           bin.install "better-whisper"
@@ -32,8 +32,8 @@ class BetterWhisper < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/akash-joshi/better-whisper/releases/download/v0.1.10/better-whisper_0.1.10_linux_arm64.tar.gz"
-        sha256 "fdc9ac46db26c03dbca09923fee9f67228402826a1c1c9a3e7746513d661a198"
+        url "https://github.com/akash-joshi/better-whisper/releases/download/v0.1.11/better-whisper_0.1.11_linux_arm64.tar.gz"
+        sha256 "b1071ba8df460446d8f693c187d9881eb99a1f64db6ccfe58fe6ed35fbffb624"
 
         def install
           bin.install "better-whisper"
